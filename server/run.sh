73caec09c6e49f86098ce05a70dc77f125a16592
Gleb Smirnov <me@gvsmirnov.ru>
2019-02-25 01:47:11 +0800

    Make server handle SIGTERM so it quits immediately when container is stopped

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-iperf3 -s -f K --one-off
+trap finish SIGTERM SIGINT
 
+function finish {
+    kill -SIGTERM $IPERF3_PID
+}
+
+iperf3 -s -f K --one-off &
+IPERF3_PID=$!
+
+wait $IPERF3_PID
